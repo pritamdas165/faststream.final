c2263b5f9ce77699777ea333bc4b1563c9b89419
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-16 21:39:28 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,35 @@
- const API_KEY = "cc9374659de08b939499a50af4715216";
-const IMG = "https://image.tmdb.org/t/p/w500";
+const API_KEY = "cc9374659de08b939499a50af4715216";
+const BASE_URL = "https://api.themoviedb.org/3";
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
-const moviesDiv = document.getElementById("movies");
+const moviesEl = document.getElementById("movies");
 
-fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
-  .then(res => res.json())
-  .then(data => {
-    data.results.forEach(movie => {
-      const div = document.createElement("div");
-      div.className = "movie";
-      div.innerHTML = `
-        <img src="${IMG + movie.poster_path}">
-        <h3>${movie.title}</h3>
-      `;
-      div.onclick = () => {
-        window.location.href = `movie.html?id=${movie.id}`;
-      };
-      moviesDiv.appendChild(div);
+async function loadMovies() {
+  const res = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await res.json();
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  moviesEl.innerHTML = "";
+
+  movies.forEach(movie => {
+    if (!movie.poster_path) return;
+
+    const div = document.createElement("div");
+    div.className = "movie";
+
+    div.innerHTML = `
+      <img src="${IMG_URL + movie.poster_path}" alt="${movie.title}">
+      <h3>${movie.title}</h3>
+    `;
+
+    div.addEventListener("click", () => {
+      window.location.href = `movie.html?id=${movie.id}`;
     });
+
+    moviesEl.appendChild(div);
   });
+}
+
+loadMovies();
