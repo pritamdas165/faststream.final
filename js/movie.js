3fea86658e1b50d32f42a13e09981383709729df
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-16 21:43:58 +0530

    Update movie.js

--- js/movie.js
+++ js/movie.js
@@ -1,4 +1,4 @@
-const API_KEY = "YOUR_TMDB_API_KEY";
+const API_KEY = cc9374659de08b939499a50af4715216";
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
