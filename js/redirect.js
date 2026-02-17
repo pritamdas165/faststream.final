b1ef1977ea45e4a686606a8f77775386d30c72d8
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-17 17:22:52 +0530

    Update redirect.js

--- js/redirect.js
+++ js/redirect.js
@@ -1,8 +1,34 @@
+/*************************
+  GET MOVIE ID FROM URL
+**************************/
 const params = new URLSearchParams(window.location.search);
-const id = params.get("id");
+const movieId = params.get("id");
 
-if (!id) {
+if (!movieId) {
+  alert("Invalid movie link");
   window.location.href = "index.html";
-} else {
-  window.location.href = "movie.html?id=" + id;
 }
+
+/*************************
+  CLICK ANALYTICS (OPEN)
+**************************/
+let analytics = JSON.parse(localStorage.getItem("clickAnalytics")) || {};
+
+if (!analytics[movieId]) {
+  analytics[movieId] = {
+    title: movieId,
+    watch: 0,
+    download: 0,
+    open: 0
+  };
+}
+
+analytics[movieId].open++;
+localStorage.setItem("clickAnalytics", JSON.stringify(analytics));
+
+/*************************
+  REDIRECT TO MOVIE PAGE
+**************************/
+setTimeout(() => {
+  window.location.href = "movie.html?id=" + movieId;
+}, 500);
