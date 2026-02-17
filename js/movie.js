ded396bc3320166ca391ada84db5f7622d00fe9e
pritamdas165 <76564815+pritamdas165@users.noreply.github.com>
2026-02-17 19:20:48 +0530

    Update movie.js

--- js/movie.js
+++ js/movie.js
@@ -2,8 +2,8 @@
    CONFIG
 ========================= */
 const API_KEY = "cc9374659de08b939499a50af4715216"; // <-- নিজের TMDB key বসাও
-const BASE_URL = "https://api.themoviedb.org/3";
-const IMG_URL = "https://image.tmdb.org/t/p/w500";
+const BASE_URL = https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=API_KEY
+ "https://image.tmdb.org/t/p/w500";
 const PLACEHOLDER = "https://via.placeholder.com/300x450?text=No+Image";
 
 /* =========================
